94ce765c3f849ed47ecc099d852ea9841df514d9
Maxime Princelle <maxime@princelle.org>
2022-02-03 13:58:48 +0100

    deploy stuff

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = function datepick(id_div, min=null, max=null) {
+  if (typeof id_div !== "string") throw new TypeError("datepick needs a id of where to put the component...");
+
+
+
+
+  
+};
